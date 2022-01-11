bb24c4f80b681cedc5af499bddfb8cae03a923d8
Kush Agarwal <agarwalkushajay@gmail.com>
2022-01-11 17:28:46 +0530

    Added Calculate method for Binary and Octal Conversion

--- src/client/converters/fromdecimal/FromDecimal.java
+++ src/client/converters/fromdecimal/FromDecimal.java
@@ -1,11 +1,38 @@
 package client.converters.fromdecimal;
 
 public class FromDecimal {
-    private double num;
-    private int base;
-
+    private final double num;
+    private final int base;
+    private int integerPart;
+    private double decimalPart;
+    private final int[] arr = new int[10];
+    private long ans;
+    private int i = 0;
     public FromDecimal(double num, int base) {
         this.num = num;
         this.base = base;
+        integerPart = (int)Math.floor(num);
+        decimalPart = num - integerPart;
+        calculate();
+    }
+
+    @Override
+    public String toString() {
+        return "FromDecimal{" +
+                "ans=" + ans +
+                '}';
+    }
+
+    void calculate() {
+        while(integerPart != 0) {
+            arr[i++] = integerPart % base;
+            integerPart /= base;
+        }
+
+        for (int i = 10 - 1; i >= 0; i--) {
+            ans = ans * 10 + arr[i];
+        }
+
+        System.out.println(ans);
     }
 }
