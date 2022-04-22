dd38463a12586213c3ea9a282a9de0357aaeff00
Kush Agarwal <agarwalkushajay@gmail.com>
2022-04-22 08:08:09 +0530

    Made Optimization Changes

--- src/client/converters/fromdecimal/FromDecimal.java
+++ src/client/converters/fromdecimal/FromDecimal.java
@@ -9,6 +9,7 @@ public class FromDecimal {
     private final int[] arr = new int[HelperConstants.ARRAY_LENGTH];
     private String ans = "";
     private int digits = 0;
+    private final char[] hexNumbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     public FromDecimal(double num, int base) {
         this.base = base;
@@ -33,22 +34,22 @@ public class FromDecimal {
             if(base == 2 || base == 8) {
                 ans += Integer.toString(arr[i]);
             } else if(base == 16) {
-                ans += printHex(arr[i]);
+                ans = ans.concat(Character.toString(hexNumbers[arr[i]]));
             }
         }
         if(decimalPart != 0) {
             ans += '.';
             for (int i = 0; i < HelperConstants.PRECISION; i++) {
                 a = (int) Math.floor(decimalPart * base);
-                ans += printHex(a);
+                ans = ans.concat(Character.toString(hexNumbers[a]));
                 decimalPart = (decimalPart * base) - a;
             }
         }
     }
 
-    char printHex(int a) {
-        return (a == 1) ? '1' : (a == 2) ? '2' : (a == 3) ? '3' : (a == 4) ? '4' : (a == 5) ? '5' : (a == 6) ? '6' : (a == 7) ? '7' :
-                (a == 8) ? '8' : (a == 9) ? '9' : (a == 10) ? 'A' : (a == 11) ? 'B' : (a == 12) ? 'C' : (a == 13) ? 'D' : (a == 14) ? 'E' :
-                        (a == 15) ? 'F' : '0';
-    }
+//    char printHex(int a) {
+//        return (a == 1) ? '1' : (a == 2) ? '2' : (a == 3) ? '3' : (a == 4) ? '4' : (a == 5) ? '5' : (a == 6) ? '6' : (a == 7) ? '7' :
+//                (a == 8) ? '8' : (a == 9) ? '9' : (a == 10) ? 'A' : (a == 11) ? 'B' : (a == 12) ? 'C' : (a == 13) ? 'D' : (a == 14) ? 'E' :
+//                        (a == 15) ? 'F' : '0';
+//    }
 }
